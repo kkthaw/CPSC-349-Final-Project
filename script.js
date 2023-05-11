fceb80c68e9246f7abe21fee085d675f01af2448
kkthaw <123521750+kkthaw@users.noreply.github.com>
2023-05-10 19:16:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("timothylee").innerHTML = "Phone: (714)765-9876";
+document.getElementById("michaelwatt").innerHTML = "Phone: (714)453-1254";
+document.getElementById("jennysmith").innerHTML = "Phone: (714)456-2378";
